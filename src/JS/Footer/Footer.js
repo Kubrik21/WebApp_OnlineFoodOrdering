c9c21e0cdaa2226ed5a232d5852ec17bb3872e49
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-08-25 16:06:41 +0200

    Footer

---
+++ src/JS/Footer/Footer.js
@@ -0,0 +1,12 @@
+import '../../CSS/Footer.css'
+
+function Footer()
+{
+    return(
+    <div className="Footer">
+        <h6>Copyright Olejnik Jakub 2022</h6>
+    </div>
+    )
+}
+
+export {Footer}
